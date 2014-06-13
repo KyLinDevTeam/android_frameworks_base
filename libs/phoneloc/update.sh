0f45e81e4323d8c942f8cd0ff6843df9a9279302
zerojim kylin <kylinmod@gmail.com>
2014-06-13 16:13:16 +0800

    cm modify

--- libs/phoneloc/update.sh
+++ libs/phoneloc/update.sh
@@ -18,6 +18,6 @@
 
 make
 ./generateData Mps -c kylin-phoneloc.txt
-cp kylin-phoneloc.dat $dirname $(dirname $(dirname $(dirname $(dirname $(pwd)))))/vendor/kylin/prebuilt/common/media/kylin-phoneloc.dat
+cp kylin-phoneloc.dat $dirname $(dirname $(dirname $(dirname $(dirname $(pwd)))))/vendor/cm/prebuilt/common/media/kylin-phoneloc.dat
 make clean
 rm kylin-phoneloc.dat
